8f2cd050ac949aa34631917c4346296012d11e42
José Pedro Sousa <outgoing@zpedro.dev>
2025-02-17 16:58:59 +0000

    mocking non-eligible addrs too

--- packages/vite/src/hooks/useEligibleAddresses.tsx
+++ packages/vite/src/hooks/useEligibleAddresses.tsx
@@ -25,14 +25,12 @@ export function useEligibleAddresses(addresses: Address[] | undefined): UseEligi
     //   address => merkleTree.indexOf(BigInt(address)) !== -1,
     // ) as `0x${string}`[];
 
-    // For now, we don't have a list of non-eligible addresses
-    // This would typically come from your application's state or another source
-    const nonEligibleAddresses: string[] =
-      addresses?.filter(address => merkleTree.indexOf(BigInt(address)) === -1) ?? [];
+    // const nonEligibleAddresses: string[] =
+    //   addresses?.filter(address => merkleTree.indexOf(BigInt(address)) === -1) ?? [];
 
     return {
       eligibleAddresses: ['0xe28b4B0C08a5A43529030A2F8444E4AEC00C9813'], // mock for demo
-      nonEligibleAddresses,
+      nonEligibleAddresses: addresses ?? [], // mock for demo
       isLoading: false,
     };
   }, [merkleTree, addresses]);
